6b879a11a5a948d135ea1b1792a4ebedd576bbde
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-11-07 20:17:28 +0100

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -16,7 +16,10 @@ end)
 local ChickenCommand = Handler.New()
 ChickenCommand:SetName("chicken")
 ChickenCommand:SetFunction(function(MSG, Args, Raw)
-    MSG:reply({content = "Here you go have some chicken!",
+
+	local PossibleMessages = {"Here you go have some chicken!", "Not for you!", "Got A Bucket O Chicken", "NOM NOM NOM"}
+
+    MSG:reply({content = PossibleMessages[math.random(1, #PossibleMessages)],
 	embed = {image = {url = "https://i.pinimg.com/originals/e8/36/7a/e8367a927b1859ea50028a031ea4b996.png"}}})
 end)
 
